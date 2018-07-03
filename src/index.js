faaacde3691e4dfcd78d0990f634dfe8d6de50e4
James O'Brien <jamesobrien.web@gmail.com>
2018-07-03 16:24:19 +0100

    [IMPL] Adds Posts Route for C-PostList

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+import PostList from './components/PostList';
+
+
+
+import './styles/css/index.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/posts" component={PostList} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
