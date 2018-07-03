87a4a30520757a41bc3b67b34980b16f337fa2c0
James O'Brien <jamesobrien.web@gmail.com>
2018-07-03 16:32:20 +0100

    [IMPL] Get PostByID for post single - gets post/user/comments

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -15,3 +15,17 @@ export async function getPosts() {
       .map(post => Object.assign({}, post, { user: userMap[post.userId].name }));
 }
 
+
+export async function getPostById(id) {
+    const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(response => response.json());
+
+    const user = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        .then(response => response.json());
+
+    const comments = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        .then(response => response.json());
+
+
+    return Object.assign({}, post, { user, comments });
+}
