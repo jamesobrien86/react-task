ec7887b9db10b99b6854247bc6b71d900928f421
James O'Brien <jamesobrien.web@gmail.com>
2018-07-03 20:46:44 +0100

    [IMPL] Add react router redirect for posts page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PostList from './components/PostList';
 import PostSingle from './components/PostSingle';
 
@@ -13,6 +13,7 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <Router>
     <Switch>
+      <Redirect exact path="/" to="/posts" />
       <Route exact path="/posts" component={PostList} />
       <Route exact path="/posts/:postId" component={PostSingle} />
     </Switch>
